76a99e26d1e5e96bf339fca8b3285337b2f42218
Miles Hilliard <skywiredvt@gmail.com>
2024-10-10 19:19:48 -0400

    fixed shell script

--- installer.sh
+++ installer.sh
@@ -18,20 +18,8 @@ apt-get install -y python3 python3-pip git rich
 print_message "Installing the rich library..."
 pip3 install rich
 
-# Download the installer.py script from GitHub
-INSTALLER_URL="https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPOSITORY/main/installer.py"  # Replace with your GitHub URL
-INSTALLER_FILE="installer.py"
-
-print_message "Downloading installer.py..."
-if curl -s -o "$INSTALLER_FILE" "$INSTALLER_URL"; then
-    print_message "Downloaded installer.py successfully."
-else
-    echo -e "\033[1;31mFailed to download installer.py.\033[0m"
-    exit 1
-fi
-
 # Run the installer.py script
 print_message "Running installer.py..."
-python3 "$INSTALLER_FILE"
+python3 installer.py
 
 print_message "Installation process completed!"
