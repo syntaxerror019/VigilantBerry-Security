453ee91f0ea53fde47d09a61bd592067f311a23b
Miles Hilliard <skywiredvt@gmail.com>
2024-11-07 17:46:16 -0500

    vigiberry: global execution bug fixed

--- vigiberry.sh
+++ vigiberry.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if ! source venv/bin/activate; then
-	echo "Failed to activate virtual environment. Have you run the installer script?"
-	exit 1
-fi
+# Activate the virtual environment
+source "$BASE_DIR/venv/bin/activate" || { echo "Failed to activate virtual environment."; exit 1; }
 
-python3 main.py
+# Run the Python script
+python3 "$BASE_DIR/main.py" || { echo "Failed to run main.py"; exit 1; }
 
+# Deactivate the virtual environment
 deactivate
